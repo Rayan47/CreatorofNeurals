5bc38a54cd638d1aaa9f85f8aa76f134c0c25ab9
Rayan47 <RayanShahanas@gmail.com>
2018-03-18 20:25:32 +0530

    Update CreatorofNeurals.py

--- CreatorofNeurals.py
+++ CreatorofNeurals.py
@@ -1,25 +1,28 @@
+import numpy as np
+
 class Creator:
+    
     #Creates neural networks based on given specs
     #Network layers to be input
     #enter() to be handled by parent class
-    import numpy as np
     
     
-    def __init__(self, layerData, inputs):
+    
+    def __init__(self, layer_data, inp):
         self.la = []
         self.wl = []
         self.bl = []
         c = 0
-        for v in layerData:
+        for v in layer_data:
             try:
-                self.la.append(self.np.asmatrix(self.np.zeros(v).reshape(v, 1)))
-                self.wl.append(self.rmat(layerdata[c + 1], v))
-                self.bl.append(self.rmat(layerData[c + 1], 1))
+                self.la.append(np.asmatrix(np.zeros(v).reshape(v, 1)))
+                self.wl.append(self.rmat(layer_data[c + 1], v))
+                self.bl.append(self.rmat(layer_data[c + 1], 1))
                 c += 1
             except IndexError:
                 pass
         del v, c
-        self.ip(inputs)
+        self.ip(inp)
         
     def ip(self, inpu):
         c2 = 0
@@ -30,15 +33,15 @@ class Creator:
         
     def rmat(self, x, y):
         lim = x * y
-        retv = self.np.asmatrix(self.np.random.rand(lim))
+        retv = np.asmatrix(np.random.rand(lim))
         for i in range(lim):
-            k = self.np.random.randint(0, 3)
+            k = np.random.randint(0, 3)
             if k == 1:
                 retv[0, (i - 1)] *= -1
         retv = retv.reshape(x, y)
         return retv
     def sig(self, x):
-        x = self.np.exp(x)/(self.np.exp(x) + 1)
+        x = np.exp(x)/(np.exp(x) + 1)
         return x               
         
     def exunt(self):
@@ -51,9 +54,9 @@ class Creator:
     def compute(self):
         no = len(self.la)
         for i in range(len(self.la) - 1):
-            new = self.np.dot(self.wl[i], self.la[i])
+            new = np.dot(self.wl[i], self.la[i])
             new = new  + self.bl[i]
-            mfunc = self.np.vectorize(self.sig, otypes=[self.np.float])
+            mfunc = np.vectorize(self.sig, otypes=[np.float])
             new = mfunc(new)
             self.la[i + 1] = new
         return self.la[no - 1]
